10a6c6cc8f55fe4dcf8c51218e274c26926dd33a
rony <rniumo@gmail.com>
2022-10-20 22:38:47 +0600

    10:38pm

--- classroom/static/classroom/scripts/classroom_dash.js
+++ classroom/static/classroom/scripts/classroom_dash.js
@@ -1,3 +1,6 @@
+let prev_page_url
+let next_page_url
+
 function activate_option_btns() {
     op_btns = $('.option-toggle')
     $.each(op_btns, function (indexInArray, valueOfElement) { 
@@ -34,27 +37,19 @@ $('#share-link').on('click', function(){
 //     }
 //  }
 
-/* <div class="post-container" id="post-container">
-      
-    </div>
-    <div class="paginator">
-      <div class="inner">
-        <ul>
-            <li><button class="paginator-btn" id="first_page"><</button></li>
-            <li><button class="paginator-btn" id="prev_page">1</button></li>
-            <li><span class="current-page">2</span></li>
-            <li><button class="paginator-btn" id="next_page">3</button></li>
-            <li><button class="paginator-btn" id="next_page">></button></li>
-        </ul>
-      </div>
-    </div> */
 
 
-function render_post_component(post_data) {
+function render_post_component(post_data, hidden=false) {
+    let inline_style
+    if (hidden) {
+        inline_style =  `style="display:none;"`
+    } else {
+        inline_style =  ''
+    }
     let description = post_data['description'].split(' ').slice(0, 7).join(' ')
     let date_raw = new Date(post_data['posted'])
     let date = date_raw.toLocaleString("en-US")
-    post_comp = `<div class="post-item">
+    post_comp = `<div class="post-item" ${inline_style}>
                     <div class="icon"><i class='bx bx-file'></i></div>
                     <div class="contents">
                     <a href="" class="title">${description}</a>
@@ -75,9 +70,13 @@ function render_post_component(post_data) {
     return post_comp
 }
 
-function render_paginator(response) {
+function render_paginator(response, is_first_load=false) {
     if (response['total_pages'] < 2) {
         return ""
+    }
+    let inline_css = ""
+    if (is_first_load) {
+        inline_css = `style="opacity:0;"`
     }
     let list_items = ""
     let current_page_no = response['current_page']
@@ -88,33 +87,76 @@ function render_paginator(response) {
             list_items += `<li><button class="paginator-btn" id="first_page"><</button></li>`
         }
         list_items += `<li><button class="paginator-btn" id="prev_page">${prev_page_no}</button></li>`
+    } else {
+        list_items += `<li style="display:none;"><button class="paginator-btn" id="first_page"><</button></li>`
+        list_items += `<li style="display:none;"><button class="paginator-btn" id="prev_page"></button></li>`
     }
-    list_items += `<li><span class="current-page">${current_page_no}</span></li>`
+    list_items += `<li><span class="current-page" id="current-page">${current_page_no}</span></li>`
     if (response['next'] !== null) {
         list_items += `<li><button class="paginator-btn" id="next_page">${next_page_no}</button></li>`
         if (next_page_no < response['total_pages']) {
-            list_items += `<li><button class="paginator-btn" id="next_page">></button></li>`
+            list_items += `<li><button class="paginator-btn" id="last_page">></button></li>`
         }
+    } else {
+        list_items += `<li style="display:none;"><button class="paginator-btn" id="next_page"></button></li>`
+        list_items += `<li style="display:none;"><button class="paginator-btn" id="last_page">></button></li>`
     }
-    let paginator =`<div class="paginator">
+    let paginator =`<div class="paginator" id="id_paginator" ${inline_css}>
                         <div class="inner">
-                        <ul>
-                            ${list_items}
-                        </ul>
+                            <ul>
+                                ${list_items}
+                            </ul>
                         </div>
                     </div>`
     return paginator
 }
 
 function render_post_container(posts_arr) {
-  let posts = ""
-  for (let post of posts_arr) {
-    posts += render_post_component(post)
-  }
-  posts_container =`<div class="post-container" id="post-container">
-                      ${posts}
-                    </div>`
-  return posts_container
+    let posts = ""
+    for (let post of posts_arr) {
+        posts += render_post_component(post)
+    }
+    posts_container =`<div class="post-container" id="post-container">
+                        ${posts}
+                        </div>`
+    return posts_container
+}
+
+function fetch_new_page(url) {
+    $.ajax({
+        type: "get",
+        url: url,
+        dataType: "json",
+        success: function (response) {
+            let container = render_post_container(response['results'])
+            let paginator = render_paginator(response)
+            let content = container + paginator
+            $("#recent-posts").html(content)
+            setTimeout(function(){ 
+                activate_option_btns()
+                activate_paginator_btns()
+             }, 50)
+            prev_page_url = response['prev']
+            next_page_url = response['next']
+        }
+    });
+}
+
+function activate_paginator_btns() {
+    $('#first_page').on('click', function(){
+        let url = `/classroom/api/${classroom_id}/posts`
+        fetch_new_page(url)
+    })
+    $('#last_page').on('click', function(){
+        let url = `/classroom/api/${classroom_id}/posts?p=last`
+        fetch_new_page(url)
+    })
+    $('#prev_page').on('click', function(){
+        fetch_new_page(prev_page_url)
+    })
+    $('#next_page').on('click', function(){
+        fetch_new_page(next_page_url)
+    })
 }
 
 
@@ -123,16 +165,19 @@ function load_page_data() {
     $.ajax({
         type: "get",
         url: url,
-        data: "data",
         dataType: "json",
         success: function (response) {
           let container = render_post_container(response['results'])
-          let paginator = render_paginator(response)
+          let paginator = render_paginator(response, true)
           let content = container + paginator
           $("#recent-posts").html(content)
           $("#recent-posts").show(500, function(){
             activate_option_btns()
+            $("#id_paginator").css('opacity', 1)
           })
+          prev_page_url = response['prev']
+          next_page_url = response['next']
+          activate_paginator_btns()
         }
     });
 }
