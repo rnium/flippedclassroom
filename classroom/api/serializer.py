3e12fd74d3de33cd0708822562fd9407113865e6
rony <rniumo@gmail.com>
2022-10-21 14:55:42 +0600

    2:55pm

--- classroom/api/serializer.py
+++ classroom/api/serializer.py
@@ -1,12 +1,23 @@
-from symtable import Class
 from rest_framework import serializers
 from classroom.models import ClassroomPost
 
 class PostSerializer(serializers.ModelSerializer):
     num_attachments = serializers.SerializerMethodField()
+    action_permitted = serializers.SerializerMethodField()
     class Meta:
         model = ClassroomPost
         exclude = ['author', 'classroom']
+
     def get_num_attachments(self, obj):
         return obj.postattachment_set.count()
+
+    def get_action_permitted(self, obj):
+        request = self.context.get('request')
+        if hasattr(request, 'user'):
+            if request.user in obj.classroom.teachers.all():
+                return True
+            else:
+                return False
+        else:
+            return False
         
