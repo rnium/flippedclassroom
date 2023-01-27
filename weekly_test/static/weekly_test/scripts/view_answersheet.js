de9e2ff969d640c75430806961e672329af24d29
rony <rniumo@gmail.com>
2023-01-28 03:09:33 +0600

    score divs activate

--- weekly_test/static/weekly_test/scripts/view_answersheet.js
+++ weekly_test/static/weekly_test/scripts/view_answersheet.js
@@ -7,6 +7,32 @@ function adjust_answercounts() {
     }
 }
 
+function activate_score_box(){
+    let score_divs = $(".q-marks-input");
+    for (let sd of score_divs) {
+        $(`#${sd.id}`).keyup(function(){
+            let value = $(this).val();
+            let marks_raw = $(this).attr("data-q-marks");
+            if (value.length > 0) {
+                let score = Number(value);
+                let marks = Number(marks_raw);
+                if (isNaN(score) || score>marks || score<0) {
+                    $(this).addClass("error")
+                } else {
+                    $(this).removeClass("error")
+                }
+            } else {
+                $(this).removeClass("error")
+            }
+        })
+    }
+}
+
+
+
+
+
 $(document).ready(function () {
     adjust_answercounts()
+    activate_score_box()
 });
