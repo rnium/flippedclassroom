028185c5f34fd9751b6d08142acef0a13b3163bf
rony <rniumo@gmail.com>
2023-01-28 01:32:37 +0600

    view answ temp rendering

--- weekly_test/admin.py
+++ weekly_test/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from weekly_test.models import WeeklyTest, AnswerSheet, Question
+from weekly_test.models import WeeklyTest, AnswerSheet, Question, McqAnswer, DescriptiveAnswer, McqOption
 # Register your models here.
 admin.site.register(WeeklyTest)
 admin.site.register(AnswerSheet)
 admin.site.register(Question)
+admin.site.register(McqAnswer)
+admin.site.register(DescriptiveAnswer)
+admin.site.register(McqOption)
