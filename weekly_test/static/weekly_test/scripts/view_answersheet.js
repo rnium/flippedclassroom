ad8b06abf0671d7269c4a047dd9ec57b63ef0e8c
rony <rniumo@gmail.com>
2023-01-28 11:36:11 +0600

    view answsheet js

--- weekly_test/static/weekly_test/scripts/view_answersheet.js
+++ weekly_test/static/weekly_test/scripts/view_answersheet.js
@@ -28,6 +28,36 @@ function activate_score_box(){
     }
 }
 
+function checkErrorFields() {
+    let fields = $("input.error")
+    if(fields.length>0){
+        for(let i=0;i<fields.length;i++){
+            let field_id = fields[i].id
+            $(`#${field_id}`).focus()
+            return false
+        }
+    } else {
+        return true
+    }
+}
+
+
+function processData(){
+    let score_divs = $(".q-marks-input");
+    let data_list = []
+    $.each(score_divs, function (indexInArray, valueOfElement) { 
+         unit_data = {}
+         let pk_raw = $(`#${valueOfElement.id}`).attr("data-ans-id")
+         let pk = Number(pk_raw)
+         let score_raw = $(`#${valueOfElement.id}`).val()
+         let score = Number(score_raw)
+         unit_data['pk'] = pk
+         unit_data['score'] = score
+         data_list.push(unit_data)
+    });
+    return data_list
+}
+
 
 
 
