4888ce391bf2bc0630ee57f6c298ef27a7f2d9bf
rony <rniumo@gmail.com>
2023-02-02 23:48:47 +0600

    update classroom

--- classroom/api/urls.py
+++ classroom/api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('newclassroom', views.create_classroom, name="create_classroom_api"),
     path('<str:pk>/posts', views.ClassroomPostsView.as_view(), name="classroom_posts"),
     path('<str:pk>/update', views.UpdateClassroomAV.as_view(), name="update_classroom"),
+    # path('<str:pk>/edit', views.update_classroom_info, name="update_classroom"),
     path('<str:pk>/removestudent', views.remove_student, name="remove_student"),
     path('post/<str:pk>/postcomment', views.post_comment, name="post_comment"),
     path('post/<str:pk>/update', views.update_post_des_and_rm_files, name="update_post_des_and_rm_files"),
---
+++ classroom/static/classroom/scripts/edit_classroom.js
@@ -0,0 +1,53 @@
+function validate_fields(){
+    let fields = $(".inp_input")
+    for (let field of fields) {
+        let field_id = field.id
+        let inp_value = $(`#${field_id}`).val()
+        if (inp_value.length === 0) {
+            $(`#${field_id}`).focus()
+            return false
+        }
+    }
+    return true
+}
+
+
+function get_payload() {
+    let raw_data = {
+        name: $("#classname").val(),
+        course: $("#course").val()
+    }
+    payload = JSON.stringify(raw_data)
+    return payload
+}
+
+
+$("#update-btn").on("click", function(){
+    let field_is_valid = validate_fields()
+    if (field_is_valid === false) {
+        return null
+    }
+    let payload = get_payload()
+    $.ajax({
+        type: "patch",
+        url: update_clssroom_api,
+        dataType: "json",
+        contentType: "application/json",
+        beforeSend: function(xhr){
+            $(`#update-btn`).attr("disabled", true)
+            xhr.setRequestHeader("X-CSRFToken", csrftoken)
+        },
+        data: payload,
+        cache: false,
+        success: function(response) {
+            $("#new-classroom-name").text(response['name'])
+            $("#form-container").hide(100, ()=>{
+                $("#success-container").show(100)
+            })
+        },
+        error: function() {
+            alert("Something went wrong")
+            $(`#update-btn`).removeAttr("disabled");
+        },
+    });
+})
