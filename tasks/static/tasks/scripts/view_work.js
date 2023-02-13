5e57038262b301a3c415d949a186a66b0b731a38
rony <rniumo@gmail.com>
2023-02-13 23:04:51 +0600

    remarks script

--- tasks/static/tasks/scripts/view_work.js
+++ tasks/static/tasks/scripts/view_work.js
@@ -43,6 +43,55 @@ function update_test_score() {
     })
 }
 
+
+function activate_edit_des_btn() {
+    $(`#remarks-info-edit`).on('click', ()=>{
+        let infoTextID = "remarks-text-raw"
+        let infoTextContainerId = "remarks-text-con"
+        let editor_id = "remarks-adder-editor"
+        let prev_info = $(`#${infoTextID}`).text()
+        $(`#${infoTextContainerId}`).hide(0, function(){
+           $(`#${editor_id}`).show()
+           $(`#${editor_id} .editor-textarea`).text(prev_info)
+           $(`#${editor_id} .editor-textarea`).focus()
+        })
+    })
+}
+
+
+
+function activate_remarks_adder() {
+    $(`#remarks_adder`).on('click', ()=>{
+        let editorId = "remarks-adder-editor";
+        let self_container_id = "remarks_adder_btn_con"
+        $(`#${self_container_id}`).hide()
+        $(`#${editorId}`).show()
+        $(`#${editorId} .editor-textarea`).focus()
+    })
+}
+
+function activate_close_editor() {
+    $(`#close-remarks-editor`).on('click', ()=>{
+        let infoTextId = "remarks-text-con"
+        let adderId = "remarks_adder_btn_con"
+        let rawTextId = "remarks-text-raw"
+        let self_container_id = "remarks-adder-editor"
+        let infoText = $(`#${rawTextId}`).text()
+        if (infoText.length > 0) {
+            $(`#${self_container_id}`).hide(0, function(){
+                $(`#${infoTextId}`).show()
+            })
+        } else {
+            $(`#${self_container_id}`).hide(0, function(){
+                $(`#${adderId}`).show()
+            })
+        }
+     })
+}
+
 $(document).ready(function () {
     $("#update_score_btn").on('click', update_test_score)
+    activate_edit_des_btn()
+    activate_remarks_adder()
+    activate_close_editor()
 });
