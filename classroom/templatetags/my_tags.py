344e898cc9c6937d7235a64957457029a47ad958
rony <rniumo@gmail.com>
2023-03-01 01:35:07 +0600

    css class filter

--- classroom/templatetags/my_tags.py
+++ classroom/templatetags/my_tags.py
@@ -8,3 +8,11 @@ def get_score_or_pending(score):
         return "Pending"
     else:
         return score
+    
+    
+@register.filter
+def get_total_score_css_class(score):
+    if score == None:
+        return "pending"
+    else:
+        return ""
