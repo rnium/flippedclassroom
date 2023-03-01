ed2a611bb104c1d4e6583fba301d8d9beb9ef166
rony <rniumo@gmail.com>
2023-03-02 02:27:44 +0600

    user activities

--- classroom/templatetags/my_tags.py
+++ classroom/templatetags/my_tags.py
@@ -1,10 +1,21 @@
 from django import template
+from django.db.models import Q
 from classroom.models import Classroom
 from tasks.models import Task, Work
 from weekly_test.models import AnswerSheet
 
 register = template.Library()
 
+@register.filter
+def prettify_marks(marks_raw):
+    if marks_raw == None:
+        return None
+    marks_int = int(marks_raw)
+    if marks_int != marks_raw:
+        return round(marks_raw, 2)
+    else:
+        return marks_int
+    
 @register.filter
 def get_score_or_pending(score):
     if score == None:
@@ -38,11 +49,8 @@ def get_test_participations(user, classroom):
 
 @register.simple_tag
 def get_task_participations(user, classroom):
-    qs = Work.objects.filter(user=user, 
-                            test__weekly__classroom=classroom, 
-                            issue_time__isnull=False, 
-                            submit_time__isnull=False)
-    qs.order_by('test.created')
+    qs = Work.objects.filter( Q(group__members=user) | Q(group=None, submission_by=user), task__classroom=classroom)
+    qs.order_by('task.addded')
     return qs
 
 
